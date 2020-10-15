44f24733022d64b6934d78fa1caf667620ecc9e1
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2020-10-14 19:11:06 -0700

    removed unused function

--- index.js
+++ index.js
@@ -77,16 +77,6 @@ const viewAllRoles = () => {
         .then(() => startApplication());
 }
 
-const viewAllEmployeesByDepartment = () => {
-    const sql = `SELECT * FROM department`;
-
-    const sql2 = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee
-        LEFT JOIN role ON employee.role_id = role.id
-        LEFT JOIN department ON role.department_id = department.id
-        LEFT JOIN employee manager ON manager.id = employee.manager_id
-    `
-}
-
 const addADept = () => {
     return inquirer.prompt([
         {
